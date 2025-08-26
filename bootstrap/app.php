35f24a45a541143228c5c13d9076b10ac5be2d76
Дмитрий Пуртов <dmitriypur@MacBook-Pro-Dmitrij.local>
2025-08-26 16:14:52 +0300

    edit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'botman',
             'api/*',
+            'livewire/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {

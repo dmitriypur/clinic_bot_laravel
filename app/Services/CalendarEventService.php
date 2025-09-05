8dcfd02afcd6e00feb8b25b571e69c175fa6f511
Дмитрий Пуртов <dmitriypur@MacBook-Pro-Dmitrij.local>
2025-09-05 14:41:12 +0300

    Убрано версионирование ID событий календаря
    - Убран _v + time() из ID событий
    - Это может решить проблему с подменой application_id
    - Теперь ID событий будут стабильными

--- app/Services/CalendarEventService.php
+++ app/Services/CalendarEventService.php
@@ -200,7 +200,7 @@ class CalendarEventService
         }
         
         $eventData = [
-            'id' => 'slot_' . $shift->id . '_' . $slot['start']->format('Y-m-d_H-i') . ($application ? '_app_' . $application->id : '') . '_v' . time(),
+            'id' => 'slot_' . $shift->id . '_' . $slot['start']->format('Y-m-d_H-i') . ($application ? '_app_' . $application->id : ''),
             'title' => $title,
             'start' => $slot['start'],
             'end' => $slot['end'],
